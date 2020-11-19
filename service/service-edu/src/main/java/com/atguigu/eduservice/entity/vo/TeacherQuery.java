ba02f4eafe53ca3d90aeda32be7de5f988490e48
lwl1216191815 <long20150509@163.com>
2020-11-19 10:23:36 +0800

    分页查询参数对象

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/entity/vo/TeacherQuery.java
@@ -0,0 +1,36 @@
+package com.atguigu.eduservice.entity.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Teacher查询对象
+ */
+@Data
+public class TeacherQuery implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 讲师名称
+     */
+    @ApiModelProperty(value = "教师名称,模糊查询")
+    private String name;
+    /**
+     * 讲师级别
+     */
+    @ApiModelProperty(value = "头衔 1高级讲师 2首席讲师")
+    private Integer level;
+
+    /**
+     * 开始时间
+     */
+    @ApiModelProperty(value = "查询开始时间", example = "2019-01-01 10:10:10")
+    private String begin;//注意，这里使用的是String类型，前端传过来的数据无需进行类型转换
+
+    /**
+     * 结束时间
+     */
+    @ApiModelProperty(value = "查询结束时间", example = "2019-12-01 10:10:10")
+    private String end;
+}
