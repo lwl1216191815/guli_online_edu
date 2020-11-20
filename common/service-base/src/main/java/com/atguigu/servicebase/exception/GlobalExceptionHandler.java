81607d271c756ebd0af842f499b648fc659bda3c
lwl1216191815 <long20150509@163.com>
2020-11-20 14:25:18 +0800

    统一异常处理类

---
+++ common/service-base/src/main/java/com/atguigu/servicebase/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.atguigu.servicebase.exception;
+
+import com.baomidou.mybatisplus.extension.api.R;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.failed("系统出现问题，请联系管理员");
+    }
+}
