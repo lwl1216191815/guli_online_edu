df7a706e4ec1e0224b8223d29fd5c996eb10666a
lwl1216191815 <long20150509@163.com>
2020-11-24 15:31:54 +0800

    全局异常处理记入日志

--- common/service-base/src/main/java/com/atguigu/servicebase/exception/handler/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/com/atguigu/servicebase/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.atguigu.servicebase.exception.handler;
 
 import com.atguigu.servicebase.exception.GuliException;
 import com.atguigu.common.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 全局异常处理
  */
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     /**
@@ -20,6 +22,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R error(Exception e){
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error().message("执行了通用异常处理");
     }
@@ -32,6 +35,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ArithmeticException.class)
     @ResponseBody
     public R error(ArithmeticException e){
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error().message("出现了除数为0的异常");
     }
@@ -44,6 +48,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(GuliException.class)
     @ResponseBody
     public R error(GuliException e){
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error().message(e.getMsg()).code(e.getCode());
     }
