a1fbcf400e6ab39a435afe5292ccad0b1b92ff99
lwl1216191815 <long20150509@163.com>
2021-02-18 16:31:00 +0800

    前端工程

---
+++ vue-admin-template-master/src/api/edu/teacher.js
@@ -0,0 +1,38 @@
+import request from '@/utils/request'
+
+export default{
+    /**
+     * 查询讲师列表
+     * @param {*} page 当前页码 
+     * @param {*} limit 每页记录数 
+     * @param {*} teacherQuery 查询条件
+     */
+    getTeacherListPage(page,limit,teacherQuery){
+        return request({
+            url: `eduservice/edu-teacher/pageCondition/${page}/${limit}`,
+            method: 'post',
+            data: teacherQuery
+          })
+    },
+    /**
+     * 根据ID删除讲师
+     * @param {*} id 讲师的ID
+     */
+    deleteTeacherId(id){
+        return request({
+            url:`eduservice/edu-teacher/${id}`,
+            method:'delete'
+        });
+    },
+    /**
+     * 保存讲师的方法
+     * @param {*} teacher 讲师对象
+     */
+    addTeacher(teacher){
+        return request({
+            url:`eduservice/edu-teacher/addTeacher`,
+            method:'post',
+            data:teacher
+        });
+    }
+}
