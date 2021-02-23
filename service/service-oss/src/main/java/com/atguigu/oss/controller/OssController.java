59d9cedabadb16e393b00768f146ae1072464bf3
lwl1216191815 <long20150509@163.com>
2021-02-23 08:50:00 +0800

    跨域注解

--- service/service-oss/src/main/java/com/atguigu/oss/controller/OssController.java
+++ service/service-oss/src/main/java/com/atguigu/oss/controller/OssController.java
@@ -2,7 +2,10 @@ package com.atguigu.oss.controller;
 
 import com.atguigu.common.utils.R;
 import com.atguigu.oss.service.IOssService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +13,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/eduOss/fileOss")
+@Api("文件上传，阿里云oss控制器")
+@CrossOrigin
 public class OssController {
     @Autowired
     private IOssService ossService;
 
     @PostMapping
+    @ApiOperation("上传用户头像，然后返回头像的url")
     public R uploadAvatar(MultipartFile file){
         String url = ossService.uploadAvatar(file);
         return R.ok().data("url",url);
