363561e1b39f71a47842eec91245161dedf0299f
lwl1216191815 <long20150509@163.com>
2021-02-25 09:47:04 +0800

    mapper

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/mapper/EduVideoMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.mapper;
+
+import com.atguigu.eduservice.entity.EduVideo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程视频（章节中的每一小节） Mapper 接口
+ * </p>
+ *
+ * @author liuwenlong
+ * @since 2021-02-24
+ */
+public interface EduVideoMapper extends BaseMapper<EduVideo> {
+
+}
