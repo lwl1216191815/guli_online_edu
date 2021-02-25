a56499c52b2a9b8abe71aac40e7f5c935cb551ce
lwl1216191815 <long20150509@163.com>
2021-02-25 09:16:58 +0800

    课程章节controller

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
@@ -0,0 +1,23 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程章节表 前端控制器
+ * </p>
+ *
+ * @author liuwenlong
+ * @since 2021-02-24
+ */
+@RestController
+@RequestMapping("/eduservice/edu-chapter")
+@CrossOrigin
+public class EduChapterController {
+
+}
+
