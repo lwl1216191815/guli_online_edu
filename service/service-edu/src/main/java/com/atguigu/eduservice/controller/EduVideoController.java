d83dda1878822974ef18cffb77a8ba1a78d28078
lwl1216191815 <long20150509@163.com>
2021-02-25 09:21:47 +0800

    课程小节controller

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -0,0 +1,23 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程视频（章节中的每一小节） 前端控制器
+ * </p>
+ *
+ * @author liuwenlong
+ * @since 2021-02-24
+ */
+@RestController
+@RequestMapping("/eduservice/edu-video")
+@CrossOrigin
+public class EduVideoController {
+
+}
+
