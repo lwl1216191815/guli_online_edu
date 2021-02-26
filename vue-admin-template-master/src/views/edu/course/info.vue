fd275bbf49fe124b49838a707dea85ef3f7d04bc
lwl1216191815 <long20150509@163.com>
2021-02-26 08:53:10 +0800

    课程信息页面构建

--- vue-admin-template-master/src/views/edu/course/info.vue
+++ vue-admin-template-master/src/views/edu/course/info.vue
@@ -6,27 +6,56 @@
       <el-step title="创建课程大纲"/>
       <el-step title="提交审核"/>
     </el-steps>
+
     <el-form label-width="120px">
+      <el-form-item label="课程标题">
+        <el-input v-model="courseInfo.title" placeholder=" 示例：机器学习项目课：从基础到搭建项目视频课程。专业名称注意大小写"/>
+      </el-form-item>
+      <!-- 所属分类 TODO -->
+      <!-- 课程讲师 TODO -->
+      <el-form-item label="总课时">
+        <el-input-number :min="0" v-model="courseInfo.lessonNum" controls-position="right" placeholder="请填写课程的总课时数"/>
+      </el-form-item>
+      <!-- 课程简介 TODO -->
+      <el-form-item label="课程简介">
+        <el-input v-model="courseInfo.description" placeholder=" 示例：机器学习项目课：从基础到搭建项目视频课程。专业名称注意大小写"/>
+      </el-form-item>
+      <!-- 课程封面 TODO -->
+      <el-form-item label="课程价格">
+        <el-input-number :min="0" v-model="courseInfo.price" controls-position="right" placeholder="免费课程请设置为0元"/> 元
+      </el-form-item>
       <el-form-item>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next">保存并下一步</el-button>
+        <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存并下一步</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
+  import courseApi from '@/api/edu/course';
   export default {
     data() {
       return {
-        saveBtnDisabled: false // 保存按钮是否禁用
+        saveBtnDisabled: false, // 保存按钮是否禁用
+        courseInfo:{}
       }
     },
     created() {
       console.log('info created')
     },
     methods: {
-      next() {
-        console.log('next')
-        this.$router.push({path: '/edu/course/chapter/1'})
+      saveOrUpdate(){
+        courseApi.addCourse(this.courseInfo).then(
+          response => {
+            this.$message({
+              type:'success',
+              message:'添加课程成功'
+            });
+            this.next(response.data.courseId);
+          }
+        ).catch();
+      },
+      next(id) {
+        this.$router.push({path: `/edu/course/chapter/${id}`})
       }
     }
   }
