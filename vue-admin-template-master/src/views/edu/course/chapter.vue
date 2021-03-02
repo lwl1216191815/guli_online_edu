e0d97fc540137a88241638b09a3bedab9ba117c8
lwl1216191815 <long20150509@163.com>
2021-03-02 09:21:28 +0800

    显示章节和小节列表

--- vue-admin-template-master/src/views/edu/course/chapter.vue
+++ vue-admin-template-master/src/views/edu/course/chapter.vue
@@ -6,6 +6,40 @@
       <el-step title="创建课程大纲"/>
       <el-step title="提交审核"/>
     </el-steps>
+    <el-button type="text" @click="openChapterDialog()">添加章节</el-button>
+
+    <!-- 章节 -->
+    <ul class="chanpterList">
+      <li
+        v-for="chapter in chapterVideoList"
+        :key="chapter.id">
+        <p>
+          {{ chapter.title }}
+
+          <span class="acts">
+                    <el-button style="" type="text" @click="openVideo(chapter.id)">添加小节</el-button>
+                    <el-button style="" type="text" @click="openEditChatper(chapter.id)">编辑</el-button>
+                    <el-button type="text" @click="removeChapter(chapter.id)">删除</el-button>
+                </span>
+        </p>
+
+        <!-- 视频 -->
+        <ul class="chanpterList videoList">
+          <li
+            v-for="video in chapter.children"
+            :key="video.id">
+            <p>{{ video.title }}
+
+              <span class="acts">
+
+                    <el-button style="" type="text">编辑</el-button>
+                    <el-button type="text" @click="removeVideo(video.id)">删除</el-button>
+                </span>
+            </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
     <el-form label-width="120px">
       <el-form-item>
         <el-button @click="previous">上一步</el-button>
@@ -15,14 +49,20 @@
   </div>
 </template>
 <script>
+  import chapterApi from '@/api/edu/chapter'
   export default {
     data() {
       return {
-        saveBtnDisabled: false // 保存按钮是否禁用
+        saveBtnDisabled: false, // 保存按钮是否禁用
+        courseId:'',
+        chapterVideoList:[],
       }
     },
     created() {
-      console.log('chapter created')
+      if(this.$router.params && this.$router.params.id){
+        this.courseId = this.$router.params.id;
+        this.getChapterVideo(this.courseId);
+      }
     },
     methods: {
       previous() {
@@ -32,7 +72,35 @@
       next() {
         console.log('next')
         this.$router.push({path: '/edu/course/publish/1'})
+      },
+      getChapterVideo(courseId){
+        chapterApi.getChapterVideoTree(courseId).then(
+          response => {
+            this.chapterVideoList = response.data.list;
+          }
+        ).catch();
+      },
+      /**
+       * 打开modal对话框
+       */
+      openChapterDialog(){
+
+      },
+      /**
+       * 删除小节
+       */
+      removeVideo(){
+
+      },
+      /**
+       * 打开小节
+       */
+      openVideo(){},
+      openEditChatper(){},
+      removeChapter(){
+
       }
+
     }
   }
 </script>
