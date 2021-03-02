00e3c88e864351c0244ad07a6a2a1e70d87191fa
lwl1216191815 <long20150509@163.com>
2021-03-02 09:03:52 +0800

    加入subjectParentId属性

--- service/service-edu/src/main/java/com/atguigu/eduservice/entity/vo/CourseVo.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/entity/vo/CourseVo.java
@@ -14,31 +14,27 @@ public class CourseVo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "课程ID")
-
     private String id;
 
     @ApiModelProperty(value = "课程讲师ID")
-
     private String teacherId;
 
-    @ApiModelProperty(value = "课程专业ID")
-
+    @ApiModelProperty(value = "课程专业二级ID")
     private String subjectId;
 
-    @ApiModelProperty(value = "课程标题")
+    @ApiModelProperty(value = "课程专业一级ID")
+    private String subjectParentId;
 
+    @ApiModelProperty(value = "课程标题")
     private String title;
 
     @ApiModelProperty(value = "课程销售价格，设置为0则可免费观看")
-
     private BigDecimal price;
 
     @ApiModelProperty(value = "总课时")
-
     private Integer lessonNum;
 
     @ApiModelProperty(value = "课程封面图片路径")
-
     private String cover;
 
     @ApiModelProperty(value = "课程简介")
