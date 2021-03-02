b07302ddb3c380f7d536d6f68ef6f37e9d025df0
lwl1216191815 <long20150509@163.com>
2021-03-02 09:06:46 +0800

    章节service实现类

--- service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
@@ -1,10 +1,20 @@
 package com.atguigu.eduservice.service.impl;
 
 import com.atguigu.eduservice.entity.EduChapter;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.entity.vo.CourseTreeNode;
 import com.atguigu.eduservice.mapper.EduChapterMapper;
 import com.atguigu.eduservice.service.EduChapterService;
+import com.atguigu.eduservice.service.EduVideoService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +26,33 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChapter> implements EduChapterService {
+    @Autowired
+    private EduVideoService eduVideoService;
 
+    @Override
+    public List<CourseTreeNode> getTreeByCourseId(String courseId) {
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("course_id",courseId);
+        List<EduChapter> chapterList = baseMapper.selectList(wrapper);
+        List<EduVideo> videoList = eduVideoService.list(wrapper);
+        List<CourseTreeNode> res = new ArrayList<>();
+        for (EduChapter chapter : chapterList) {
+            CourseTreeNode node = new CourseTreeNode();
+            BeanUtils.copyProperties(chapter,node);
+            Iterator<EduVideo> it = videoList.iterator();
+            List<CourseTreeNode> children = new ArrayList<>();
+            while (it.hasNext()){
+                EduVideo next = it.next();
+                if(next.getChapterId().equals(chapter.getId())){
+                    CourseTreeNode childNode = new CourseTreeNode();
+                    BeanUtils.copyProperties(next,childNode);
+                    children.add(childNode);
+                    it.remove();
+                }
+            }
+            node.setChildren(children);
+            res.add(node);
+        }
+        return res;
+    }
 }
