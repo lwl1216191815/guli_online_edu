8ee916e05e6510595f51e73d2e8ebed51f293462
lwl1216191815 <long20150509@163.com>
2021-03-03 09:17:51 +0800

    1 根据ID获取课程信息 2 修改课程信息

--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
@@ -6,6 +6,7 @@ import com.atguigu.eduservice.entity.vo.CourseVo;
 import com.atguigu.eduservice.service.EduCourseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,11 +26,40 @@ public class EduCourseController {
     @Autowired
     private EduCourseService eduCourseService;
 
+    /**
+     * 添加课程
+     * @param vo 参数对象
+     * @return
+     */
     @PostMapping("/addCourse")
     @ApiOperation("添加课程")
     public R addCourse(@RequestBody CourseVo vo){
         String courseId = eduCourseService.saveCourse(vo);
         return R.ok().data("courseId",courseId);
+    }
+
+    /**
+     * 根据课程ID获取课程信息
+     * @param courseId
+     * @return
+     */
+    @GetMapping("/getCourseById/{courseId}")
+    @ApiOperation("根据课程ID获取课程信息")
+    public R getCourseById(@PathVariable @ApiParam("课程id") String courseId){
+        CourseVo vo = eduCourseService.selectCourseVoById(courseId);
+        return R.ok().data("data",vo);
+    }
+
+    /**
+     * 修改课程信息
+     * @param vo
+     * @return
+     */
+    @PutMapping("/updateCourse")
+    @ApiOperation("修改课程信息")
+    public R updateCourse(@RequestBody CourseVo vo){
+        eduCourseService.updateCourseByVo(vo);
+        return R.ok();
     }
 }
 
