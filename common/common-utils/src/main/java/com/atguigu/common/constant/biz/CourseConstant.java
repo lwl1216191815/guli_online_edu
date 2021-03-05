3075eae9db6ed741146164376d941415ddd7ef9d
lwl1216191815 <long20150509@163.com>
2021-03-05 13:15:51 +0800

    课程实体类

---
+++ common/common-utils/src/main/java/com/atguigu/common/constant/biz/CourseConstant.java
@@ -0,0 +1,11 @@
+package com.atguigu.common.constant.biz;
+
+/**
+ * 课程业务常量类
+ */
+public class CourseConstant {
+    /**
+     * 课程已发布状态
+     */
+    public static final String HAD_PUBLISHED = "Normal";
+}
