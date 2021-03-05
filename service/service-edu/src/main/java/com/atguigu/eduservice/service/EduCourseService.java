9e8ff4e37abfd5d50ffc30d66d048c97ef55094a
lwl1216191815 <long20150509@163.com>
2021-03-05 13:47:30 +0800

    1 获取课程发布信息 2 自定义分页查询课程信息

--- service/service-edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
@@ -1,7 +1,12 @@
 package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.query.CourseQuery;
+import com.atguigu.eduservice.entity.vo.CourseListVo;
+import com.atguigu.eduservice.entity.vo.CoursePublishVo;
 import com.atguigu.eduservice.entity.vo.CourseVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -31,4 +36,19 @@ public interface EduCourseService extends IService<EduCourse> {
      * @param vo
      */
     void updateCourseByVo(CourseVo vo);
+
+    /**
+     * 根据课程ID获取课程发布信息
+     * @param courseId
+     * @return
+     */
+    CoursePublishVo getCoursePublishInfo(String courseId);
+
+    /**
+     * 自定义分页条件查询课程列表
+     * @param coursePage 分页对象
+     * @param query 查询条件
+     * @return
+     */
+    IPage<CourseListVo> pageList(Page<CourseListVo> coursePage, CourseQuery query);
 }
