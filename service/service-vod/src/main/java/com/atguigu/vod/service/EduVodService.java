d238ea4a5df86ab6486d4cac0f9ce97865b8b3bf
lwl1216191815 <long20150509@163.com>
2021-03-13 10:11:27 +0800

    阿里云视屏点播Service

---
+++ service/service-vod/src/main/java/com/atguigu/vod/service/EduVodService.java
@@ -0,0 +1,15 @@
+package com.atguigu.vod.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 阿里云视屏点播
+ */
+public interface EduVodService {
+    /**
+     * 向阿里云中上传视屏，返回视屏ID
+     * @param file
+     * @return
+     */
+    String uploadVideo(MultipartFile file);
+}
