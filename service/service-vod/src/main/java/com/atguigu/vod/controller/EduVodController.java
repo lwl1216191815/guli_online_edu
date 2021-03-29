4a3a47261ffc12a9305f52e7edf5307590d944ee
lwl1216191815 <long20150509@163.com>
2021-03-29 16:05:37 +0800

    删除阿里云中的视屏

--- service/service-vod/src/main/java/com/atguigu/vod/controller/EduVodController.java
+++ service/service-vod/src/main/java/com/atguigu/vod/controller/EduVodController.java
@@ -4,15 +4,15 @@ import com.atguigu.common.utils.R;
 import com.atguigu.vod.service.EduVodService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/eduVod/vod")
 @Api(tags = "视屏上传，阿里云控制controller")
+@CrossOrigin
 public class EduVodController {
     @Autowired
     private EduVodService eduVodService;
@@ -22,5 +22,12 @@ public class EduVodController {
     public R uploadVideo(MultipartFile file){
         String videoId = eduVodService.uploadVideo(file);
         return R.ok().data("videoId",videoId);
+    }
+
+    @DeleteMapping("{videoId}")
+    @ApiOperation("删除视屏")
+    public R deleteVideo(@PathVariable("videoId") @ApiParam(name = "videoId",value = "云端视屏ID",required = true) String videoId){
+        eduVodService.removeVideo(videoId);
+        return R.ok().message("删除视屏成功");
     }
 }
