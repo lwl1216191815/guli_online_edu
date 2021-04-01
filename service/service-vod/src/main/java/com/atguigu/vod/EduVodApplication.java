783cfdc3b87db5c02921be5272448c1199858cf1
lwl1216191815 <long20150509@163.com>
2021-04-01 11:17:59 +0800

    集成nacos

--- service/service-vod/src/main/java/com/atguigu/vod/EduVodApplication.java
+++ service/service-vod/src/main/java/com/atguigu/vod/EduVodApplication.java
@@ -3,10 +3,12 @@ package com.atguigu.vod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan("com.atguigu")
+@EnableDiscoveryClient
 public class EduVodApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduVodApplication.class,args);
