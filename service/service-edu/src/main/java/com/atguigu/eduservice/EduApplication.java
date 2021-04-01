2a14dfe01fcce8b80ad75cbf6db4bf53a3c91778
lwl1216191815 <long20150509@163.com>
2021-04-01 11:12:01 +0800

    集成nacos和feign

--- service/service-edu/src/main/java/com/atguigu/eduservice/EduApplication.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -2,10 +2,14 @@ package com.atguigu.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.atguigu")
+@EnableDiscoveryClient//表示可以通过nacos提供服务
+@EnableFeignClients//表示可以通过feign调用服务
 public class EduApplication {
 
     public static void main(String[] args) {
