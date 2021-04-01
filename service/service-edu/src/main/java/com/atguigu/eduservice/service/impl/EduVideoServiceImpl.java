10cf6b0524e57407690339e14a717f240938c131
lwl1216191815 <long20150509@163.com>
2021-04-01 11:09:27 +0800

    完成删除阿里云上的视频

--- service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduVideoServiceImpl.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduVideoServiceImpl.java
@@ -1,12 +1,16 @@
 package com.atguigu.eduservice.service.impl;
 
+import com.atguigu.eduservice.client.VodClient;
 import com.atguigu.eduservice.entity.EduVideo;
 import com.atguigu.eduservice.mapper.EduVideoMapper;
 import com.atguigu.eduservice.service.EduVideoService;
-import com.atguigu.servicebase.exception.GuliException;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -18,14 +22,33 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EduVideoServiceImpl extends ServiceImpl<EduVideoMapper, EduVideo> implements EduVideoService {
-    // TODO 还得删除视屏
+    @Autowired
+    private VodClient vodClient;
+
     @Override
     public void deleteByCourseId(String courseId) {
         QueryWrapper<EduVideo> wrapper = new QueryWrapper();
         wrapper.eq("course_id",courseId);
-        boolean result = remove(wrapper);
-        if(!result){
-            throw new GuliException(20001,"删除课时失败");
+        List<EduVideo> list = list(wrapper);
+        for (EduVideo video : list) {
+            deleteAll(video);
         }
+    }
+
+    @Override
+    public void deleteById(String videoId) {
+        EduVideo video = getById(videoId);
+        if(video != null && StringUtils.isNotBlank(video.getVideoSourceId())){
+            deleteAll(video);
+        }
+    }
+
+    /**
+     * 删除课时以及视屏
+     * @param video
+     */
+    private void deleteAll(EduVideo video){
+        vodClient.deleteVideo(video.getVideoSourceId());
+        removeById(video.getId());
     }
 }
