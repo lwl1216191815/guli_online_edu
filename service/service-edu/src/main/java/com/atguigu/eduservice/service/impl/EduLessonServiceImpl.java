ffc9a72026e4ced4867042106b6ffad87f5afa35
lwl1216191815 <long20150509@163.com>
2021-04-02 17:13:52 +0800

    video改lesson

--- service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduLessonServiceImpl.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduLessonServiceImpl.java
@@ -1,9 +1,9 @@
 package com.atguigu.eduservice.service.impl;
 
 import com.atguigu.eduservice.client.VodClient;
-import com.atguigu.eduservice.entity.EduVideo;
-import com.atguigu.eduservice.mapper.EduVideoMapper;
-import com.atguigu.eduservice.service.EduVideoService;
+import com.atguigu.eduservice.entity.EduLesson;
+import com.atguigu.eduservice.mapper.EduLessonMapper;
+import com.atguigu.eduservice.service.EduLessonService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.apache.commons.lang3.StringUtils;
@@ -21,34 +21,34 @@ import java.util.List;
  * @since 2021-02-24
  */
 @Service
-public class EduVideoServiceImpl extends ServiceImpl<EduVideoMapper, EduVideo> implements EduVideoService {
+public class EduLessonServiceImpl extends ServiceImpl<EduLessonMapper, EduLesson> implements EduLessonService {
     @Autowired
     private VodClient vodClient;
 
     @Override
     public void deleteByCourseId(String courseId) {
-        QueryWrapper<EduVideo> wrapper = new QueryWrapper();
+        QueryWrapper<EduLesson> wrapper = new QueryWrapper();
         wrapper.eq("course_id",courseId);
-        List<EduVideo> list = list(wrapper);
-        for (EduVideo video : list) {
-            deleteAll(video);
+        List<EduLesson> list = list(wrapper);
+        for (EduLesson lesson : list) {
+            deleteAll(lesson);
         }
     }
 
     @Override
     public void deleteById(String videoId) {
-        EduVideo video = getById(videoId);
-        if(video != null && StringUtils.isNotBlank(video.getVideoSourceId())){
-            deleteAll(video);
+        EduLesson lesson = getById(videoId);
+        if(lesson != null && StringUtils.isNotBlank(lesson.getVideoSourceId())){
+            deleteAll(lesson);
         }
     }
 
     /**
      * 删除课时以及视屏
-     * @param video
+     * @param lesson
      */
-    private void deleteAll(EduVideo video){
-        vodClient.deleteVideo(video.getVideoSourceId());
-        removeById(video.getId());
+    private void deleteAll(EduLesson lesson){
+        vodClient.deleteVideo(lesson.getVideoSourceId());
+        removeById(lesson.getId());
     }
 }
