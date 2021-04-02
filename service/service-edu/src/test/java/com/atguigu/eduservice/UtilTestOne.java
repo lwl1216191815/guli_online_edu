055f9952fa48dd272a3e5b7b7a3dcfd1efb220c9
lwl1216191815 <long20150509@163.com>
2021-04-02 17:19:46 +0800

    单元测试

--- service/service-edu/src/test/java/com/atguigu/eduservice/UtilTestOne.java
+++ service/service-edu/src/test/java/com/atguigu/eduservice/UtilTestOne.java
@@ -3,6 +3,10 @@ package com.atguigu.eduservice;
 import cn.hutool.core.util.StrUtil;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 相关工具类测试
  */
@@ -13,4 +17,22 @@ public class UtilTestOne {
         System.out.println(s.length());
     }
 
+    @Test
+    public void testLong(){
+        double a = 5l;
+        double b = 2l;
+        System.out.println(a/b);
+    }
+    @Test
+    public void testDate(){
+        //long x = 1617330929915l;
+        long x =1617256678000l;
+        Date date = new Date(x);
+        DateFormat df = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+        DateFormat df1 = new SimpleDateFormat("yyyyMMddHHmmss");
+        String s = df.format(date);
+        String s2 = df1.format(date);
+        System.out.println(s);
+        System.out.println(s2);
+    }
 }
