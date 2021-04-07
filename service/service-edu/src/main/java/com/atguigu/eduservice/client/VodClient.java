44d1c0ef21418f6474c4191c788063c1728488d2
lwl1216191815 <long20150509@163.com>
2021-04-07 09:09:13 +0800

    集成hystrix熔断机制，设置fallback

--- service/service-edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
@@ -2,13 +2,14 @@ package com.atguigu.eduservice.client;
 
 
 import com.atguigu.common.utils.R;
+import com.atguigu.eduservice.client.impl.VodClientImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient("service-vod")
+@FeignClient(name="service-vod",fallback = VodClientImpl.class)
 public interface VodClient {
     /**
      * 根据视屏ID删除阿里云上的视屏
