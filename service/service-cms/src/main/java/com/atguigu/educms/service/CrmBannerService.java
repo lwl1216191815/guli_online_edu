228d2f59a6dc666e53c96db5d419386cd2a0eb5f
lwl1216191815 <long20150509@163.com>
2021-04-14 14:51:04 +0800

    轮播图service

---
+++ service/service-cms/src/main/java/com/atguigu/educms/service/CrmBannerService.java
@@ -0,0 +1,16 @@
+package com.atguigu.educms.service;
+
+import com.atguigu.educms.entity.CrmBanner;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 首页banner表 服务类
+ * </p>
+ *
+ * @author liuwenlong
+ * @since 2021-04-08
+ */
+public interface CrmBannerService extends IService<CrmBanner> {
+
+}
