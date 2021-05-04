cd6b81be132ee357ee5e6de5d4c8b661e2138b31
lwl1216191815 <long20150509@163.com>
2021-05-04 21:16:29 +0800

    设置验证码过期时间为5分钟

--- service/service-sms/src/main/java/com/atguigu/edusms/service/impl/SMSServiceImpl.java
+++ service/service-sms/src/main/java/com/atguigu/edusms/service/impl/SMSServiceImpl.java
@@ -10,19 +10,30 @@ import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.atguigu.edusms.service.ISMSservice;
 import com.atguigu.edusms.utils.RandomUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import com.aliyuncs.exceptions.ServerException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SMSServiceImpl implements ISMSservice {
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
     @Override
     public Boolean send(String phone) {
+        //先从redis中取，如果有就直接返回，可以防止重复发送
+        String code = redisTemplate.opsForValue().get(phone);
+        if(StringUtils.isNotBlank(code)){
+            return true;
+        }
         DefaultProfile profile = DefaultProfile.getProfile("cn-qingdao", "<accessKeyId>", "<accessSecret>");
         IAcsClient client = new DefaultAcsClient(profile);
-
         CommonRequest request = new CommonRequest();
         request.setSysMethod(MethodType.POST);
         request.setSysDomain("dysmsapi.aliyuncs.com");
@@ -30,11 +41,16 @@ public class SMSServiceImpl implements ISMSservice {
         request.setSysAction("SendSms");
         request.putBodyParameter("PhoneNumbers",phone);
         Map<String,Object> param = new HashMap<>();
-        param.put("code", RandomUtil.getFourBitRandom());
+        code = RandomUtil.getFourBitRandom();
+        param.put("code", code);
         request.putBodyParameter("TemplateCode", JSONUtil.toJsonStr(param));
         try {
             CommonResponse response = client.getCommonResponse(request);
-            return response.getHttpResponse().isSuccess();
+            boolean success = response.getHttpResponse().isSuccess();
+            if(success){
+               redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+            }
+            return success;
         } catch (ServerException e) {
             e.printStackTrace();
         } catch (ClientException e) {
