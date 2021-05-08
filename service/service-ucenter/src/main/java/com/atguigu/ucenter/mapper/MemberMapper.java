d2b0b4bf2bbed2194ab906f3df83941e9bb6cbfd
lwl1216191815 <long20150509@163.com>
2021-05-08 22:06:36 +0800

    mapper

---
+++ service/service-ucenter/src/main/java/com/atguigu/ucenter/mapper/MemberMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.ucenter.mapper;
+
+import com.atguigu.ucenter.entity.Member;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 会员表 Mapper 接口
+ * </p>
+ *
+ * @author liuwenlong
+ * @since 2021-05-06
+ */
+public interface MemberMapper extends BaseMapper<Member> {
+
+}
