8ef8d9dac4f801b5542ab3f1da843c3f7fc406d7
lwl1216191815 <long20150509@163.com>
2021-05-08 22:02:15 +0800

    mybatisPlus配置类

---
+++ service/service-ucenter/src/main/java/com/atguigu/ucenter/config/MybatisPlusConfig.java
@@ -0,0 +1,45 @@
+package com.atguigu.ucenter.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@MapperScan(basePackages = "com.atguigu.ucenter.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 逻辑删除插件
+     * @return
+     */
+    @Bean
+    public ISqlInjector sqlInjector(){
+        return new LogicSqlInjector();
+    }
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * sql性能分析插件
+     * @return
+     */
+    @Bean
+    @Profile({"dev","test"})
+    public PerformanceInterceptor performanceInterceptor(){
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        performanceInterceptor.setMaxTime(10000);
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
+    }
+}
